6b79b34478d61491839eaca25164b6ab7fef4da1
phamducdat <phamducdat2402@gmail.com>
2022-09-19 17:30:53 +0700

    update

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-const packageJson = require("./package.json");
 
+import postcss from "rollup-plugin-postcss";
+const packageJson = require("./package.json");
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 export default [
     {
         input: "src/index.ts",
@@ -21,14 +23,19 @@ export default [
             },
         ],
         plugins: [
+            peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+
+            postcss(),
         ],
     },
     {
         input: "dist/esm/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+
+        external: [/\.css$/],
     },
 ];
--- src/components/Button/DP_Button.tsx
+++ src/components/Button/DP_Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import "./Button.css";
+import {Button} from "antd";
 export interface DP_ButtonProps {
     label: string;
 }
 
 const DP_Button = (props: DP_ButtonProps) => {
-    return <button>{props.label}</button>;
+    return <Button title={"datpd"}>{"hava"}</Button>;
 };
 
 export default DP_Button;
