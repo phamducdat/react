a2a793e4e1752fe30b08857881a822a6f6cffde3
phamducdat <phamducdat2402@gmail.com>
2022-09-20 21:16:40 +0700

    update

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path') // lấy đường dẫn tuyệt đối của thư mục
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    }
+}
+module.exports = config;
