e79e23a0ad57363dc99c51cd8d23bf318d04e127
phamducdat <phamducdat2402@gmail.com>
2022-09-21 20:52:27 +0700

    init

---
+++ dist/cjs/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var Button = function (props) {
+    return React__default["default"].createElement("button", null, props.label);
+};
+
+exports.Button = Button;
+//# sourceMappingURL=index.js.map
---
+++ dist/esm/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var Button = function (props) {
+    return React.createElement("button", null, props.label);
+};
+
+export { Button };
+//# sourceMappingURL=index.js.map
