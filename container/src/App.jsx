3343083e8bdbe8bdf48793f4965a4151752fcb2b
phamducdat <phamducdat2402@gmail.com>
2022-11-25 19:53:32 +0700

    init

--- container/src/App.jsx
+++ container/src/App.jsx
@@ -1,14 +1,14 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
+import { Counter } from 'counter/Counter';
 
 import "./index.css";
 
 const App = () => (
-  <div className="container">
-    <div>Name: container</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
+    <div className="container">
+        <h1>Container App</h1>
+        <Counter />
+    </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
