44c9e472142d2e6ffb8425723fdbaf7c8b9fc99a
phamducdat2402@gmail.com <phamducdat2402@gmail.com>
2022-12-13 21:02:11 +0700

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/home";
+import {Counter} from 'counter/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <BrowserRouter>
+
+            <Routes>
+                <Route index element={<Home/>}/>
+                <Route path="home/*" element={<Home/>}/>
+                <Route path="counter/*" element={<Counter datpd={"datpd"}/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ my-app/src/components/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = props => {
+    return (
+        <div>
+            Home
+        </div>
+    );
+};
+
+Home.propTypes = {};
+
+export default Home;
